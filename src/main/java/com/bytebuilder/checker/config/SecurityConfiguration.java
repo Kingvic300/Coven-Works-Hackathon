a70f24677e6226b4f6f8f43b021e084a0f8dc809
Victor <oladimejivictor611@gmail.com>
2025-07-12 12:29:01 +0100

    deploying

--- src/main/java/com/bytebuilder/checker/config/SecurityConfiguration.java
+++ src/main/java/com/bytebuilder/checker/config/SecurityConfiguration.java
@@ -1,4 +1,5 @@
 package com.bytebuilder.checker.config;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,22 +13,26 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     @Autowired
     private JwtAuthenticationFilter jwtFilter;
+
     @Autowired
     private AuthenticationProvider authenticationProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authenticationProvider(authenticationProvider)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
                 .oauth2Login(oauth2 -> oauth2
                         .defaultSuccessUrl("/api/skill-nest/auth/users/google-callback", true)
                         .failureUrl("/api/skill-nest/auth/users/login-user?error=true")
@@ -35,6 +40,4 @@ public class SecurityConfiguration {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
 }
-
